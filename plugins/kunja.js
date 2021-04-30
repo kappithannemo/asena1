6b481bffaae86d88fee95478a129e3e9219d1e30
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-04-30 23:38:51 +0530

    Update kunja.js

--- plugins/kunja.js
+++ plugins/kunja.js
@@ -11,13 +11,13 @@ const got = require('got');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
-Asena.addCommand({pattern: 'bot ?(.*)', fromMe: false }, async (message, match) => {
+Asena.addCommand({pattern: 'kunjappa ?(.*)', fromMe: false }, async (message, match) => {
 	
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*🤖 ' + Lang.BOT +'* ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*🤖 ' + ":" +'* ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
 	} catch {
 		return await message.client.sendMessage(message.jid, "error", MessageType.text);
 	}
